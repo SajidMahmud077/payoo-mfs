7f039fcb8e628d79da406f6dcfa0801e077a001b
SajidMahmud077 <mahmudsajid366@gmail.com>
2024-09-19 13:57:43 +0600

    new balance added

--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
 // add money to the account #
 // s1:addm event handler
 // s:2 get money tobe added to the account balance
+// getthe pin number
 //s:1 add an event handler to the add money button inside the form
 document.getElementById('btn-add-money').addEventListener('click', function(event){
   // prevent page reload after form submit
@@ -11,5 +12,23 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
   // get pin number You Provided
   const pinNumberInput = document.getElementById('input-pin-number').value;
   console.log(pinNumberInput);
+  // verify pin number
+  // wrong way to validate
+  if(pinNumberInput === '1234'){
+    console.log('Adding Money To Your Account');
+    // step:4 get the dcurrent balance
+    const balance=document.getElementById('account-balance').innerText;
+    console.log(typeof balance);
+    // step:5 addMoneyInput wtih Balance
+    const addMoneyNumber= parseFloat(addmoneyInput);
+    const balanceNumber= parseFloat(balance);
+    const newBalance= addMoneyNumber + balanceNumber;
+    console.log(newBalance);
 
-});
+    // step:5 update the balance in the UI/DOM
+    document.getElementById('account-balance').innerText = newBalance;
+  }
+  else{
+    alert('failed to add money! please try again');
+  }
+  });
